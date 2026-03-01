167f200dbbb65a9856109bcdb34a8cf59c3f2dbc
Porker562 <139104915+Porker562@users.noreply.github.com>
2026-03-01 12:41:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".project-card");
+
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add("show");
+        }, index * 200);
+    });
+});
